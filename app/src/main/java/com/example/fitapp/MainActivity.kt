6c63fc7635e7e204e3fcb4250f3ee7742de9ca27
Yusuf Serdar Yazıcıoğlu <frekons@gmail.com>
2021-12-01 11:44:20 +0300

    Initial commit

---
+++ app/src/main/java/com/example/fitapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.fitapp
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.appcompat.app.AppCompatActivity
+import com.example.fitapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+}
